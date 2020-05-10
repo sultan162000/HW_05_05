44d63bb50a9a81aa72998b997bde786b1604a166
Кодиров Султон <sultonkodiro109@gmail.com>
2020-05-10 14:36:09 +0500

    Создан класс Map

--- Program.cs
+++ Program.cs
@@ -24,6 +24,18 @@ namespace ConsoleApp2
             }
 
 
+            Map<int, string> map = new Map<int, string>();
+            map.Add(10, "Mag");
+            map.Add(20, "Warrior");
+            map.Add(22, "Inquisitor");
+            map.Add(30, "Ogr");
+            map.Add(80, "Programist");
+            foreach (var item in map)
+            {
+                Console.WriteLine(item);
+            }
+
+
             
 
             Console.ReadKey();
@@ -105,6 +117,124 @@ namespace ConsoleApp2
 
        
        
+    }
+
+    public class Item<T, S>
+    {
+        
+        public T Key { get; set; }
+        public S Value { get; set; }
+
+        public Item() { }
+        public Item(T key, S value)
+        {
+            
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            Key = key;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Key.ToString() + " : " + Value.ToString();
+        }
+    }
+
+    public class Map<T, S>
+    {
+        private List<Item<T, S>> _items = new List<Item<T, S>>();
+        
+        public int Count
+        {
+            get
+            {
+                return _items.Count;
+            }
+        }
+
+        public IReadOnlyList<T> Keys
+        {
+            get
+            {
+                return _items.Select(i => i.Key).ToList();
+            }
+        }
+
+
+        public void Add(Item<T, S> item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            if (_items.Any(i => i.Key.Equals(item.Key)))
+            {
+                throw new ArgumentException($"Словарь уже содержит значение с ключом {item.Key}.", nameof(item));
+            }
+
+            _items.Add(item);
+        }
+
+
+        public void Add(T key, S value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (_items.Any(i => i.Key.Equals(key)))
+            {
+                throw new ArgumentException($"Словарь уже содержит значение с ключом {key}.", nameof(key));
+            }
+
+            var item = new Item<T, S>()
+            {
+                Key = key,
+                Value = value
+            };
+
+            // Добавляем данные в коллекцию.
+            _items.Add(item);
+        }
+        
+        
+        
+        public S Get(T key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            
+
+            var item = _items.SingleOrDefault(i => i.Key.Equals(key)) ??
+                throw new ArgumentException($"Словарь не содержит значение с ключом {key}.", nameof(key));
+
+            return item.Value;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+
     }
     
 
