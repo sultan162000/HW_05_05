1741102d6fd55d63166851f45b0bb926c0a8d426
Кодиров Султон <sultonkodiro109@gmail.com>
2020-05-10 14:13:09 +0500

    Новый проект

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_05_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
